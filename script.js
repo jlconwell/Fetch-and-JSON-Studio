50426efa9b47ee20317c8c570c7758f1f4b63b20
Jake Conwell <jakelconwell@gmail.com>
2023-03-01 13:26:59 -0600

    added code to script.js. added new class to compensate for new code in script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// TODO: add code here
+window.addEventListener("load", () => {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json){
+            let astronauts = json;
+            astronauts.sort(function(a,b) {return b.hoursInSpace - a.hoursInSpace});
+            let astroDiv = document.getElementById("container");
+            astroDiv.innerHTML = `<p>Astronaut Count: ${json.length}</p>`
+            for(let value of astronauts) {
+                astroDiv.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${value.firstName} ${value.lastName}</h2>
+                        <ul>
+                            <li ${(value.active ? "class='active'" : "")}>Active: ${value.active}</li>
+                            <li>Last Name: ${value.lastName}</li>
+                            <li>Hours in Space: ${value.hoursInSpace}</li>
+                            <li>Skills: ${value.skills}, </li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src = "${value.picture}" />
+                </div>
+                `;
+            }
+        });
+    });
+});
